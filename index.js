5ca39ef81a10a26bd9a18a540a2dec93a575b29e
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-09 22:48:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function findMatching(drivers)
  return n ;
 });
 }
+
+
+  const drivers = []
+
+  //const
+  function findMatching(arr,find)
+  {
+    return arr.filter(el => el.toLowerCase().indexOf(find.toLowerCase()) !== -1)
+  }
+//console.log(findMatching(drivers, 'Bobby'))
+function fuzzyMatch(drivers, regexp) {
+
+  drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+  return drivers.filter(function (word) {
+       return regexp.test(word);
+  });
+}
+
+console.log(fuzzyMatch(drivers, /^An/));
